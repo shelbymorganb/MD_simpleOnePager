d42394f8d24df30b08626df6baa3a8355f3250eb
shelbymorganb <shelbymorganbell@gmail.com>
2015-09-08 07:45:49 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+/*first console log*/
+    console.log('Welcome to console');
+
+	$( "#view-source" ).mouseover(function() {
+		console.log("mouse over");
+	});
+
+});
